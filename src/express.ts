f6dc0636910fb9ec26eb25e015dddf853c3ff474
Nguyen Cong Duy <ncongduy@gmail.com>
2022-03-03 18:04:45 +0700

    feat: simulate Expressjs

--- src/express.ts
+++ src/express.ts
@@ -1,7 +1,93 @@
-export class Request {}
+import http from 'http';
 
-export class Response {}
+export class Request {
+	body: {};
+
+	constructor() {
+		this.body = {};
+	}
+}
+
+export class Response {
+	statusCode: number;
+	message: string;
+
+	constructor() {
+		this.statusCode = 200;
+		this.message = '';
+	}
+
+	status(statusNumber: number) {
+		this.statusCode = statusNumber;
+		return this;
+	}
+
+	send(messageStr: string) {
+		this.message = messageStr;
+		return this;
+	}
+}
 
 export default class Express {
-  // your code goes here
+	// your code goes here
+	request: {
+		path: string;
+		method: string;
+		status: number;
+		message: string;
+	}[];
+
+	constructor() {
+		this.request = [];
+	}
+
+	get(path: string, handler: (req: Request, res: Response) => void) {
+		const request = new Request();
+		const response = new Response();
+		handler(request, response);
+
+		const newData = {
+			path,
+			method: 'GET',
+			status: response.statusCode,
+			message: response.message,
+		};
+
+		this.request = [...this.request, newData];
+	}
+
+	post(path: string, handler: (req: Request, res: Response) => void) {
+		const request = new Request();
+		const response = new Response();
+		handler(request, response);
+
+		const newData = {
+			path,
+			method: 'POST',
+			status: response.statusCode,
+			message: response.message,
+		};
+
+		this.request = [...this.request, newData];
+	}
+
+	listen(port: number) {
+		const server = http.createServer((req, res) => {
+			const method = req.method;
+			const url = req.url;
+
+			const router = this.request.find(
+				(rq) => rq.path === url && rq.method === method
+			);
+
+			if (typeof router === 'object') {
+				res.statusCode = router.status;
+				res.end(router.message);
+			} else {
+				res.end('Do not have match route.');
+			}
+		});
+
+		server.listen(port);
+	}
 }
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
-import Express, { Request, Response } from './express'
+import Express, { Request, Response } from './express';
 
-const app = new Express()
+const app = new Express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('hello')
-})
+	res.status(200).send('hello');
+});
+
+app.get('/user', (req: Request, res: Response) => {
+	res.status(200).send('hello user');
+});
 
 app.post('/', (req: Request, res: Response) => {
-  const body = req.body
-  res.status(200).send('post request')
-})
+	const body = req.body;
+	res.status(200).send('post request');
+});
 
-app.listen(3000)
+app.post('/user', (req: Request, res: Response) => {
+	const body = req.body;
+	res.status(200).send('post request user');
+});
+
+app.listen(3000);
